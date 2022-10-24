2d09ba6c3327bad83db5bde7170575d5b38ae1b2
Cleanteoullion <115983858+Cleanteoullion@users.noreply.github.com>
2022-10-24 18:56:08 +0200

    Add files via upload

---
+++ TP-FINAL/db.php
@@ -0,0 +1,14 @@
+<?php
+$server = "localhost";
+$user = "root";
+$pass = "root";
+$database = "tp-final";
+
+$connection = mysqli_connect($server, $user, $pass, $database);
+
+if (!$connection) {
+    die("Erreur");
+}
+   // ICI DOIT APPARAITRE LA CONNECTION AVEC LA BASE DE DONNEES
+   // ET LA GESTION DES ERREURS
+?>
